4b658df0373e78894390759e9bfdf470b10b6e88
Bryan Reed <bryan87reed@gmail.com>
2023-09-09 17:29:43 -0700

    bind choice buttons and add comments

--- play_test.gd
+++ play_test.gd
@@ -38,17 +38,20 @@ var p2_stats = {
 }
 
 var round = 1
+var p1_chose = false
+var p2_chose = false
+## TODO: Make dictionaries with choice logic
 var p1_choices = [
-	'[CHA] Allure',
-	'[CHA] Empower',
-	'[WIT] Insult',
-	'[WIT] Quick Retort'
+	'[CHA] Allure - DEF bonus',
+	'[CHA] Empower - CHA/WIT Bonus',
+	'[WIT] Insult - Damage',
+	'[WIT] Quick Retort - Damage*'
 ]
 var p2_choices = [
-	'[CHA] Allure',
-	'[CHA] Empower',
-	'[WIT] Insult',
-	'[WIT] Quick Retort'
+	'[CHA] Allure - DEF bonus',
+	'[CHA] Empower - CHA/WIT Bonus',
+	'[WIT] Insult - Damage',
+	'[WIT] Quick Retort - Damage*'
 ]
 
 ## PLAY TEST GAME FLOW ##
@@ -80,14 +83,27 @@ func populate_choices():
 		var button = Button.new()
 		button.text = choice
 		button.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		button.button_up.connect(on_choice_button_up.bind('p1', choice))
 		$P1Choices.add_child(button)
 	
 	for choice in p2_choices:
 		var button = Button.new()
 		button.text = choice
 		button.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		button.button_up.connect(on_choice_button_up.bind('p2', choice))
 		$P2Choices.add_child(button)
 
+func on_choice_button_up(player, choice):
+	## TODO: Check if other player has chosen, if so start outcome
+	print(player)
+	print(choice)
+	
+func calculate_outcome():
+	## TODO: Calculate outcome first based on intitiave then choices
+	## TODO: Apply changes to player stat state then call update_player_stat_labels()
+	## TODO: Increment round then repopulate choices 
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
