3f1438c4a7997444283870b7e5bd1c4f7ed33f87
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-09-11 00:48:25 -0400

    add map, player, and movement

---
+++ scripts/player_one.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+const SPEED = 2
+var idle_direction = "left"
+@onready var player_sprite = $AnimationPlayer
+
+var directions = {
+	"right": Vector2(1, 0),
+	"left": Vector2(-1, 0)
+}
+	
+var inputs = {
+	"right": ["ui_right", KEY_D],
+	"left": ["ui_left", KEY_A]
+}
+
+func _process(delta):
+	handle_input()
+
+func handle_input():
+	var movement_direction = Vector2.ZERO
+	
+	for direction in directions.keys():
+		if Input.is_action_pressed(inputs[direction][0]) or Input.is_key_pressed(inputs[direction][1]):
+			movement_direction += directions[direction]
+			idle_direction = determine_animation_suffix(directions[direction])
+			
+	if movement_direction.length() > 0:
+		movement_direction = movement_direction.normalized()
+		position += movement_direction * SPEED
+		player_sprite.play("walk_%s" % determine_animation_suffix(movement_direction))
+	else:
+		player_sprite.play("idle_%s" % idle_direction)
+		
+func determine_animation_suffix(direction: Vector2) -> String:
+	if direction == Vector2(1, 0):
+		return "right"
+	elif direction == Vector2(-1, 0):
+		return "left"
+	return ""
