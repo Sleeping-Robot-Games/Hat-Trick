6b75f75b8c846f68c46938d25099a18f91292a00
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-09-12 01:43:51 -0400

    adds a timer between 2-8 seconds to idle when walking

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -4,27 +4,47 @@ var speed = 100
 var start_pos = Vector2(128, 436)
 var end_pos = Vector2(1875, 436)
 var target_pos = start_pos
+var is_moving = true
+var last_direction = 1 # 1 for right, -1 for left
 @onready var anim_npc = $AnimationPlayer
+@onready var idle_timer = $SpriteHolder/IdleTimer
 
 func _ready():
 	global_position = start_pos
 	set_new_target()
 
 func _physics_process(delta):
-	var move_direction = (target_pos - global_position).normalized()
-	var movement = move_direction * speed * delta
-	move_and_collide(movement)
-	
-	# Play the correct walking animation based on the direction
-	if move_direction.x < 0:
-		anim_npc.play("player/walk_left")
-	elif move_direction.x > 0:
-		anim_npc.play("player/walk_right")
+	if is_moving:
+		var move_direction = (target_pos - global_position).normalized()
+		var movement = move_direction * speed * delta
+		move_and_collide(movement)
 
-	# Check if the character is close enough to the target position
-	if global_position.distance_to(target_pos) < 10:
-		set_new_target()
+		if move_direction.x < 0:
+			anim_npc.play("player/walk_left")
+			last_direction = -1
+		elif move_direction.x > 0:
+			anim_npc.play("player/walk_right")
+			last_direction = 1
+
+		# Check if character has reached its target
+		if (last_direction == 1 and global_position.x >= target_pos.x) or (last_direction == -1 and global_position.x <= target_pos.x):
+			reach_target()
+
+func reach_target():
+	is_moving = false
+	idle_timer.start(randf_range(2, 8))
+	play_idle_animation()
+
+func play_idle_animation():
+	if last_direction == -1:
+		anim_npc.play("player/idle_left")
+	elif last_direction == 1:
+		anim_npc.play("player/idle_right")
 
 func set_new_target():
 	target_pos.x = randf_range(start_pos.x, end_pos.x)
 	target_pos.y = 436
+
+func _on_idle_timer_timeout():
+	set_new_target()
+	is_moving = true
