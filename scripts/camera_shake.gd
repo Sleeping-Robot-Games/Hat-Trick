5fb43279ca3d0195c30c511698df47b966c41367
Casualty <12024874+thomasdye@users.noreply.github.com>
2023-09-16 21:22:06 -0400

    adds elevator background movement / shake

---
+++ scripts/camera_shake.gd
@@ -0,0 +1,30 @@
+extends Camera2D
+
+# Screen shake variables
+var shake_duration = 0.0
+var shake_magnitude = 0.0
+var shake_speed = 50.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.shake_camera(10, 1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _process(delta):
+	if shake_duration > 0:
+		# Calculate an offset using a random direction and the magnitude
+		var offset = Vector2(randf() * 2.0 - 1.0, randf() * 2.0 - 1.0).normalized() * shake_magnitude
+		
+		# Apply the offset to the camera's position
+		self.global_position += offset
+		
+		# Decrease the duration of the shake by the elapsed time
+		shake_duration -= delta
+	else:
+		shake_duration = 0.0
+		shake_magnitude = 0.0
+		
+func shake_camera(duration, magnitude):
+	shake_duration = duration
+	shake_magnitude = magnitude
---
+++ scripts/elevator_background.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var elevator_background = $TileMap
+
+var speed = Vector2(0, 1000) # This will move the background downward
+
+# Assuming each tile is 48 pixels tall and you have 10 tiles in height
+var tile_height = 16
+var total_tiles = 20
+var total_height = 300
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position.y = -875
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Update the position of the TileMap
+	position += speed * delta
+	
+	# If the TileMap has moved down its entire height, loop it
+	if position.y >= (total_height -100):
+		position.y = -875
